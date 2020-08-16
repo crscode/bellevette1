e0065f14ec1157739f96329b364d521f882e78bc
Christoper Smith <Christophersmithcode@gmail.com>
2020-08-15 21:58:05 -0400

    fixed all files

---
+++ src/resumeData.js
@@ -0,0 +1,111 @@
+let resumeData = {
+  "imagebaseurl":"https://crscode.github.io/",
+  "name": "Christopher R. Smith",
+  "role": "Full Stack Developer and a Mathematician",
+  "imgurl":"crscode",
+  "skypeid": "Your skypeid",
+  "roleDescription": "",
+  "socialLinks":[
+      {
+        "name":"google",
+        "url":"https://www.linkedin.com/in/crscode/",
+        "className":"fa fa-google-plus"
+      },
+      {
+        "name":"linkedin",
+        "url":"https://www.linkedin.com/in/crscode/",
+        "className":"fa fa-linkedin"
+      },
+      {
+        "name":"github",
+        "url":"https://github.com/crscode",
+        "className":"fa fa-github"
+      },
+      {
+        "name":"twitter",
+        "url":"http://twitter.com/crscode",
+        "className":"fa fa-twitter"
+      }
+    ],
+  "aboutme":"Christopher R. Smith, Experienced in explaining and demonstrating new products and concepts. Highly skilled in rapidly developing rapport. Analytical and organized with the demonstrated ability to identify needs, makes recommendations, and implement effective solutions. Extremely adaptable and able to grasp and apply new information. A team player, I am highly organized and able to coordinate multiple projects simultaneously. Innovative optimized solution seeker. Excited to be at the deployment phase of my career as a web developer. I am ambitious, adventurous, assiduous, animated, and an alliteration advocate.",
+  "address":"Atlanta, GA.",
+  "website":"",
+  "education":[
+    {
+      "UniversityName":"University of Central Florida",
+      "specialization":"Full Stack Development",
+      "MonthOfPassing":"September",
+      "YearOfPassing":"2020",
+      "Achievements":"The University of Central Florida, with its 13 colleges and 11 regional campus locations, provides opportunities to 63,000 students from all 50 states and 140 countries. UCF Coding Boot Camp, UCF Data Analytics and Visualization Boot Camp, UCF Digital Marketing Boot Camp, and UCF UX/UI Boot Camp aim to further the university’s mission to provide opportunity, offered by the UCF Division of Continuing Education. UCF Boot Camps prepare students for the fields of web development, data analytics, digital marketing, or UX/UI through a hands-on classroom environment. We empower talented instructors to teach the most in-demand skills and a dedicated support team to provide students with extensive support along the way."
+    },
+    {
+      "UniversityName":"Bethune Cookman University",
+      "specialization":"Mathematics",
+      "MonthOfPassing":"May",
+      "YearOfPassing":"2003",
+      "Achievements":"The Bachelor of Science degree offered by the Department of Mathematics and Physics allows student substantial flexibility in choosing electives outside the major. It is recommended that students seek the advice of the students' advisor regarding career opportunities and choice of a suitable minor. Mathematics education, together with proper coursework in relevant interdisciplinary areas such as computer science and engineering, environmental science, business, and economics, or the natural sciences, leads to many career opportunities."
+    }
+  ],
+  "work":[
+    {
+      "CompanyName":"Some Company",
+      "specialization":"Some specialization",
+      "MonthOfLeaving":"Jan",
+      "YearOfLeaving":"2018",
+      "Achievements":"Some Achievements"
+    },
+    {
+      "CompanyName":"Some Company",
+      "specialization":"Some specialization",
+      "MonthOfLeaving":"Jan",
+      "YearOfLeaving":"2018",
+      "Achievements":"Some Achievements"
+    }
+  ],
+  "skillsDescription":"CrsCode skills ",
+  "skills":[
+    {
+      "skillname":"html5"
+    },
+    {
+      "skillname":"CSS"
+    },
+    {
+      "skillname":"Reactjs"
+    }
+  ],
+  "portfolio":[
+    {
+      "name":"Mother Trucker",
+      "description":"Mother Trucker is an application using Sequelize, Node, Express, Express-Handlebars, and Socket.io while incorporating Mapquest, that allow users to find local Food Truck in their area and allows Food Truck owners to post/update/delete their Food Truck post..",
+      "imgurl":"images/mothertruckers.gif"
+    },
+    {
+      "name":"Work Day Scheduler",
+      "description":"Mother Trucker is an application using Sequelize, Node, Express, Express-Handlebars, and Socket.io while incorporating Mapquest, that allow users to find local Food Truck in their area and allows Food Truck owners to post/update/delete their Food Truck post..",
+      "imgurl":"images/workday.gif"
+    },
+    {
+      "name":"My Budget Tracker",
+      "description":"Giving users a fast and easy way to track their money is important, but allowing them to access that information anytime is even more important. Having offline functionality is paramount to our applications success.",  
+      "imgurl":"images/mybudgettracker.gif"
+    },
+    {
+      "name":"The Classic Genius",
+      "description":"A research app to help users collect scholar research articles from all over the world, and give user an option to enhance their study with classical music. Moreover, if the user enjoy what they are hearing, there are links to lead user to a music library with more information on the artist and song playing.",
+      "imgurl":"images/theclassicgenius.gif"
+    }
+  ],
+  "testimonials":[
+    {
+      "description":"Wow well done Chris, I am impressed. You took this to the next level in my opinion using the inputs instead of prompts. I was able to generate a password quicker then going through the prompts and confirmations",
+      "name":"Tim Sherry - TA UCF"
+    },
+    {
+      "description":"This is a sample testimonial",
+      "name":"Some technical guy"
+    }
+  ]
+}
+
+export default resumeData
